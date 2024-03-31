a53df0683259e78390510dc1525c6fe8d44d6162
SpreadiesInSpace <f16poom@gmail.com>
2024-03-31 11:34:14 +0700

    Setup Script Fixes

--- Setup-Slackware-Current-Full.sh
+++ Setup-Slackware-Current-Full.sh
@@ -10,8 +10,8 @@ sudo nano /etc/hosts
 # Grab Slackware Setup Scripts by gosh-its-arch-linux
 git clone https://gitlab.com/gosh-its-arch-linux/slackware-scripts.git
 cd slackware-scripts
-#cd Current
-cd Slackware15
+cd Current
+#cd Slackware15
 chmod +x *.sh
 # Set up Slackware User, init level 4
 # sudo ./setup_script
