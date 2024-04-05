1bb2222f3701e8a5e2736241058d056c92260be9
SpreadiesInSpace <f16poom@gmail.com>
2024-04-05 21:29:44 +0700

    Setup Script Fixes

--- Setup-Slackware-15.0.sh
+++ Setup-Slackware-15.0.sh
@@ -244,8 +244,8 @@ for group in "${groups[@]}"; do
 done
 
 # Replace specific liness in sddm.conf
-# sudo sed -i "/\[Autologin\]/,/User=/ s/User=.*/User=$username/" /etc/sddm.conf
-# sudo sed -i "/\[Autologin\]/,/Session=/ s/Session=.*/Session=cinnamon/" /etc/sddm.conf
+# sed -i "/\[Autologin\]/,/User=/ s/User=.*/User=$username/" /etc/sddm.conf
+# sed -i "/\[Autologin\]/,/Session=/ s/Session=.*/Session=cinnamon/" /etc/sddm.conf
 
 # Modify systemd configuration to change the default timeout for stopping services during shutdown, preserving old one
 # cp /etc/systemd/system.conf /etc/systemd/system.conf.old
