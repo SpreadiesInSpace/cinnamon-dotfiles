506cd302044e16c0526d41421d572f46d277cdd2
SpreadiesInSpace <f16poom@gmail.com>
2024-04-08 15:14:26 +0700

    Setup Script Fixes

--- home/Setup-LMDE-Theme.sh
+++ home/Setup-LMDE-Theme.sh
@@ -146,7 +146,8 @@ home_dir="${HOME}"
 # Define the path to JSON file
 json_file="${home_dir}/.config/cinnamon/spices/menu@cinnamon.org/0.json"
 # Use sed to replace /home/f16poom with the home directory in the value field on line 91
-sed -i "91s|\"value\": \"/home/f16poom/linuxmint-logo-filled-ring.svg\"|\"value\": \"${home_dir}/linuxmint-logo-filled-ring.svg\"|g" $json_file
+sed -i "91s|\"value\": \"/home/f16poom/linuxmint-logo-filled-ring.svg\"|\"value\": \"${home_dir}/.icons/linuxmint-logo-filled-ring.svg\"|g" $json_file
+mv ~/linuxmint-logo-filled-ring.svg ~/.icons/
 
 # Sets Wallpaper
 gsettings set org.cinnamon.desktop.background picture-uri file://${HOME}/wallpapers/Desktop_Wallpaper.png
