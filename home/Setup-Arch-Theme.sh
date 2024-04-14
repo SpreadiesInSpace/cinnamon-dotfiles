9a0b688b6d2cf97ea06a99ca81f6132a2a35e0bb
SpreadiesInSpace <f16poom@gmail.com>
2024-04-14 20:47:57 +0700

    Setup Script Fixes

--- home/Setup-Arch-Theme.sh
+++ home/Setup-Arch-Theme.sh
@@ -147,7 +147,9 @@ cd ../..
 
 # Sets Wallpaper
 gsettings set org.cinnamon.desktop.background picture-uri file://${HOME}/wallpapers/Desktop_Wallpaper.png
-gsettings set org.cinnamon.desktop.background.slideshow image-source directory://${HOME}/wallpapers
+mkdir -p ~/Pictures
+ln -s ~/wallpapers/* ~/Pictures
+gsettings set org.cinnamon.desktop.background.slideshow image-source directory://${HOME}/Pictures
 
 # Sets Login Sounds
 gsettings set org.cinnamon.sounds login-enabled true
