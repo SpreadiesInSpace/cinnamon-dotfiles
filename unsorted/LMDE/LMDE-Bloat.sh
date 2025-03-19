139f2a8276a87efa5bdc7cdc5c32938d0f69572b
Spready <f16poom@gmail.com>
2025-03-19 19:02:11 +0700

    Setup Script Fixes

--- unsorted/LMDE/LMDE-Bloat.sh
+++ unsorted/LMDE/LMDE-Bloat.sh
@@ -2,3 +2,4 @@
 
 # Remove Bloat
 sudo apt remove -y baobab bulky celluloid drawing firefox gnome-calendar gnome-disk-utility hexchat hypnotix mint-artwork mint-backgrounds* mintbackup mintstick mintupdate numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer mint-L* mint-x* mint-y*
+sudo rm -rf /usr/share/icons/Bibata-* /usr/share/icons/GoogleDot-* /usr/share/icons/XCursor-Pro-* /usr/share/icons/DMZ-* /usr/share/icons/ubuntu-mono-*
