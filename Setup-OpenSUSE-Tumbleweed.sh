cf8f86f141f3b3bedbbc1dff616975110c643f42
Spready <f16poom@gmail.com>
2025-03-19 23:19:11 +0700

    Setup Script Fixes

--- Setup-OpenSUSE-Tumbleweed.sh
+++ Setup-OpenSUSE-Tumbleweed.sh
@@ -81,6 +81,7 @@ packages=(
     "evince"
     "ffmpegthumbnailer"
     "gedit"
+    "gedit-plugin-*"
     "gedit-plugins"
     "gnome-calculator"
     "gnome-screenshot"
@@ -136,10 +137,10 @@ zypper in -y neofetch
 zypper al neofetch
 
 # Apply gedit-plugins fix
-cd unsorted/openSUSE/
-chmod +x gedit-plugins-fix.sh
-./gedit-plugins-fix.sh
-cd ../..
+# cd unsorted/openSUSE/
+# chmod +x gedit-plugins-fix.sh
+# ./gedit-plugins-fix.sh
+# cd ../..
 
 # Install Additional Tools for Virt Manager
 zypper in -y -t pattern kvm_server kvm_tools
