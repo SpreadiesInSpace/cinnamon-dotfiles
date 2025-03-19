1b6974723c14c0886359b5cca786531bdefb2c99
Spready <f16poom@gmail.com>
2025-03-19 17:27:13 +0700

    Setup Script Fixes

---
+++ unsorted/Fedora/Fedora-Bloat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Remove Bloat
+sudo dnf remove -y baobab bulky celluloid drawing firefox gnome-calendar gnome-disk-utility hexchat hypnotix mint-artwork mint-backgrounds* mintbackup mintstick mintupdate numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer eom google-noto-seriff* ibus paper-icon-theme pidgin shotwell tracker-miners xawtv xfburn
