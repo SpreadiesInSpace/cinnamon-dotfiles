c0224f849e6f0baf95afa3e57c995c2420e44e0f
Spready <f16poom@gmail.com>
2025-03-20 15:44:27 +0700

    Setup Script Fixes

--- unsorted/Slackware/Slackware-Install.sh
+++ unsorted/Slackware/Slackware-Install.sh
@@ -13,38 +13,69 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-# Prompt for root password
-# read -sp "Enter new root password: " rootpasswd
-# echo
-
-# Prompt for new user details
+# Prompt for user details
 read -p "Enter new username: " username
 read -sp "Enter password for $username: " userpasswd
 echo
 
-# BTRFS Subvolumes (for Timeshift) - Preparing the Disks
-mkfs.vfat /dev/vda1
-mkfs.btrfs -f /dev/vda2
-mount /dev/vda2 /mnt
+# Prompt for hostname
+read -p "Enter hostname: " hostname
+
+# Prompt for drive to partition
+read -p "Enter drive to use (e.g., /dev/vda, /dev/nvme0n1, /dev/mmcblk0): " drive
+
+# Partition the drive
+echo "Partitioning $drive..."
+parted -s "$drive" mklabel gpt
+parted -s "$drive" mkpart primary fat32 1MiB 513MiB
+parted -s "$drive" set 1 esp on
+parted -s "$drive" mkpart primary btrfs 513MiB 100%
+
+# Format the partitions
+if [[ "$drive" == *"nvme"* ]] || [[ "$drive" == *"mmcblk"* ]]; then
+  mkfs.vfat "${drive}p1"
+  mkfs.btrfs -f "${drive}p2"
+else
+  mkfs.vfat "${drive}1"
+  mkfs.btrfs -f "${drive}2"
+fi
+
+# Mount the partitions and create BTRFS subvolumes
+if [[ "$drive" == *"nvme"* ]] || [[ "$drive" == *"mmcblk"* ]]; then
+  mount "${drive}p2" /mnt
+else
+  mount "${drive}2" /mnt
+fi
+
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
 umount /mnt
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@ /dev/vda2 /mnt/ 
-mkdir -p /mnt/home
-mount -o noatime,compress=zstd,space_cache=v2,subvol=@home /dev/vda2 /mnt/home 
-mkdir -p /mnt/boot/efi
-mount /dev/vda1 /mnt/boot/efi
+
+if [[ "$drive" == *"nvme"* ]] || [[ "$drive" == *"mmcblk"* ]]; then
+  mount -o noatime,compress=zstd,space_cache=v2,subvol=@ "${drive}p2" /mnt
+  mkdir -p /mnt/home
+  mount -o noatime,compress=zstd,space_cache=v2,subvol=@home "${drive}p2" /mnt/home
+  mkdir -p /mnt/boot/efi
+  mount "${drive}p1" /mnt/boot/efi
+else
+  mount -o noatime,compress=zstd,space_cache=v2,subvol=@ "${drive}2" /mnt
+  mkdir -p /mnt/home
+  mount -o noatime,compress=zstd,space_cache=v2,subvol=@home "${drive}2" /mnt/home
+  mkdir -p /mnt/boot/efi
+  mount "${drive}1" /mnt/boot/efi
+fi
 
 # Setup
-# Don't accidentally format partition
-# Don't install (E)LILO
-# Enable rc.samba
-# Hardware Clock to UTC
-# Select XFCE as DE
-# Drop to Shell
+echo "Follow these steps during 'setup':
+- Do not format partitions accidentally
+- Do not install (E)LILO
+- Enable rc.samba
+- Set hardware clock to UTC
+- Select XFCE as DE
+- Drop to Shell after installation"
 setup
 
-# Copy Network Info 
+# Copy Network Info
 cp --dereference /etc/resolv.conf /mnt/etc/
 
 # Entering Chroot
@@ -55,39 +86,22 @@ source /etc/profile
 
 # Install Grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+# Set GRUB timeout to 0
+sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Edit fstab for Timeshift
+# Edit /etc/fstab for Timeshift
 # noatime,compress=zstd,space_cache=v2,subvol=@ for /
-# noatime,compress=zstd,space_cache=v2,subvol=@home for /home
-# nano /etc/fstab
-
-# Install arch-install-scripts for fstab 
-wget https://gitlab.archlinux.org/archlinux/arch-install-scripts/-/archive/v29/arch-install-scripts-v29.tar.gz
-wget https://slackbuilds.org/slackbuilds/15.0/system/arch-install-scripts.tar.gz
-tar xvf arch-install-scripts.tar.gz
-mv arch-install-scripts-v29.tar.gz arch-install-scripts/
-cd arch-install-scripts/
-./arch-install-scripts.SlackBuild
-installpkg /tmp/arch-install-scripts-29-noarch-1_SBo.tgz
-cd ..
-rm -rf arch-install-scripts*
-
-# Generate Fstab
+# noatime,compress=zstd,space_cache=v2,subvol=@home for /home # nano /etc/fstab
 genfstab -U / > /etc/fstab
 
-# Add User - Set password for root and user with responses from the start
-# echo "root:$rootpasswd" | chpasswd
-useradd -m -g users -G wheel,audio,video,plugdev,netdev,lp,scanner -s /bin/bash $username
-echo "$username:$userpasswd" | chpasswd
-
 # Set Hostname
-echo Slackware > /etc/HOSTNAME
+echo "$hostname" > /etc/HOSTNAME
 echo "# For loopbacking.
 127.0.0.1               localhost
-::1                     localhost" | sudo tee /etc/hosts > /dev/null
+::1                     localhost" | tee /etc/hosts > /dev/null
 
-# Setup Sudo by uncommenting %wheel ALL=(ALL:ALL) with visudo
+# Setup Sudo
 sed -i 's/^#\s*\(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
 
 # Set Run Level to 4
@@ -98,12 +112,15 @@ else
   echo "/etc/inittab file not found. Skipping the run level change."
 fi
 
+# Add User
+useradd -m -g users -G wheel,audio,video,plugdev,netdev,lp,scanner -s /bin/bash $username
+echo "$username:$userpasswd" | chpasswd
+
 # Clone My Repo as the new user
 cat << EOUSR | su - $username
 cd
 git clone https://github.com/SpreadiesInSpace/cinnamon-dotfiles
 cd cinnamon-dotfiles
-# sudo bash Setup-Slackware.sh
 echo "Reboot and run Setup-Slackware.sh in cinnamon-dotfiles located in $username's home folder."
 EOUSR
 EOF
