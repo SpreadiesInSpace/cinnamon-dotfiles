fd0c9a6a50472c14879daf05d781140bd4e1e6ce
Spready <f16poom@gmail.com>
2025-03-28 05:49:36 +0700

    Setup Script Fixes

--- Setup-LMDE-6.sh
+++ Setup-LMDE-6.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run the script using sudo."
+  exit
+fi
+
+# Check if the script is run from the root account
+if [ "$SUDO_USER" = "" ]; then
+  echo "Please do not run this script from the root account. Use sudo instead."
+  exit
+fi
+
 # Get the current username
-username=$(whoami)
+username=$SUDO_USER
 
 # Update system and install sudo, git and curl
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y sudo git curl
+apt update && apt upgrade -y
+apt install -y git curl
 
 # Install Bottom
 VERSION="0.10.2"
@@ -15,25 +27,25 @@ URL="https://github.com/ClementTsang/bottom/releases/download/${VERSION}/bottom_
 # Download the specified version using curl
 curl -LO "$URL"
 # Install the downloaded package
-sudo dpkg -i bottom_${FILE_VERSION}_amd64.deb
+dpkg -i bottom_${FILE_VERSION}_amd64.deb
 # Remove the downloaded package file
-sudo rm bottom_${FILE_VERSION}_amd64.deb
+rm bottom_${FILE_VERSION}_amd64.deb
 
 # Install Brave Browser
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt install -y brave-browser
 
 # Install Neovim AppImage
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
 chmod u+x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract
 ./squashfs-root/AppRun --version
-sudo rm -rf /squashfs-root/
-sudo mv squashfs-root /
-sudo rm -rf /usr/bin/nvim
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+rm -rf /squashfs-root/
+mv squashfs-root /
+rm -rf /usr/bin/nvim
+ln -s /squashfs-root/AppRun /usr/bin/nvim
 rm nvim-linux-x86_64.appimage
 
 # All packages
@@ -109,76 +121,76 @@ packages=(
 )
 
 # Update system and install packages
-sudo apt install -y "${packages[@]}"
+apt install -y "${packages[@]}"
 
 # Enable Flathub
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Preserve old libvirtd configuration (for Virtual Machine Manager)
-sudo cp /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.old
+cp /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.old
 
 # Check for 'unix_sock_group' entry
 if ! grep -q "^unix_sock_group = \"libvirt\"$" /etc/libvirt/libvirtd.conf; then
-    echo 'unix_sock_group = "libvirt"' | sudo tee -a /etc/libvirt/libvirtd.conf
+    echo 'unix_sock_group = "libvirt"' | tee -a /etc/libvirt/libvirtd.conf
 else
-    sudo sed -i '/^#*unix_sock_group = "libvirt"/s/^#*//' /etc/libvirt/libvirtd.conf
+    sed -i '/^#*unix_sock_group = "libvirt"/s/^#*//' /etc/libvirt/libvirtd.conf
 fi
 
 # Check for 'unix_sock_ro_perms' entry
 if ! grep -q "^unix_sock_ro_perms = \"0777\"$" /etc/libvirt/libvirtd.conf; then
-    echo 'unix_sock_ro_perms = "0777"' | sudo tee -a /etc/libvirt/libvirtd.conf
+    echo 'unix_sock_ro_perms = "0777"' | tee -a /etc/libvirt/libvirtd.conf
 else
-    sudo sed -i '/^#*unix_sock_ro_perms = "0777"/s/^#*//' /etc/libvirt/libvirtd.conf
+    sed -i '/^#*unix_sock_ro_perms = "0777"/s/^#*//' /etc/libvirt/libvirtd.conf
 fi
 
 # Check for 'unix_sock_rw_perms' entry
 if ! grep -q "^unix_sock_rw_perms = \"0770\"$" /etc/libvirt/libvirtd.conf; then
-    echo 'unix_sock_rw_perms = "0770"' | sudo tee -a /etc/libvirt/libvirtd.conf
+    echo 'unix_sock_rw_perms = "0770"' | tee -a /etc/libvirt/libvirtd.conf
 else
-    sudo sed -i '/^#*unix_sock_rw_perms = "0770"/s/^#*//' /etc/libvirt/libvirtd.conf
+    sed -i '/^#*unix_sock_rw_perms = "0770"/s/^#*//' /etc/libvirt/libvirtd.conf
 fi
 
 # Preserve old QEMU configuration (for Virtual Machine Manager)
-sudo cp /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.old
+cp /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.old
 
 # Check for 'user' entry
 if ! grep -q "^user = \"$username\"$" /etc/libvirt/qemu.conf; then
-    echo "user = \"$username\"" | sudo tee -a /etc/libvirt/qemu.conf
+    echo "user = \"$username\"" | tee -a /etc/libvirt/qemu.conf
 fi
 
 # Check for 'group' entry
 if ! grep -q "^group = \"$username\"$" /etc/libvirt/qemu.conf; then
-    echo "group = \"$username\"" | sudo tee -a /etc/libvirt/qemu.conf
+    echo "group = \"$username\"" | tee -a /etc/libvirt/qemu.conf
 fi
 
 # Check for 'swtpm_user' entry
 if ! grep -q "^swtpm_user = \"$username\"$" /etc/libvirt/qemu.conf; then
-    echo "swtpm_user = \"$username\"" | sudo tee -a /etc/libvirt/qemu.conf
+    echo "swtpm_user = \"$username\"" | tee -a /etc/libvirt/qemu.conf
 fi
 
 # Check for 'swtpm_group' entry
 if ! grep -q "^swtpm_group = \"$username\"$" /etc/libvirt/qemu.conf; then
-    echo "swtpm_group = \"$username\"" | sudo tee -a /etc/libvirt/qemu.conf
+    echo "swtpm_group = \"$username\"" | tee -a /etc/libvirt/qemu.conf
 fi
 
 # Enable and start the libvirtd service
-sudo systemctl enable --now libvirtd.service
+systemctl enable --now libvirtd.service
 
 # Start and autostart the default network
-sudo virsh net-start default
-sudo virsh net-autostart default
+virsh net-start default
+virsh net-autostart default
 
 # Add the current user to the necessary groups
 groups=(libvirt libvirt-qemu kvm input disk video audio)
 for group in "${groups[@]}"; do
-    sudo usermod -aG "$group" "$username"
+    usermod -aG "$group" "$username"
 done
 
 # Backs up old lightdm.conf
-sudo cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.old
+cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.old
 
 # Replace specific lines in lightdm.conf
-sudo awk -i inplace '
+awk -i inplace '
 /^\[Seat:\*\]/ {a=1}
 a==1 && /^#?greeter-hide-users=/ {
     print "greeter-hide-users=false"
@@ -200,23 +212,23 @@ a==1 && /^#?autologin-session=/ {
 ' /etc/lightdm/lightdm.conf
 
 # Create a new group named 'autologin' if it doesn't already exist
-sudo groupadd -f autologin
+groupadd -f autologin
 # Add the current user to the 'autologin' group
-sudo gpasswd -a $username autologin
+gpasswd -a $username autologin
 
 # Modify systemd configuration to change the default timeout for stopping services during shutdown via drop in file
-sudo mkdir -p /etc/systemd/system.conf.d
-echo "[Manager]" | sudo tee /etc/systemd/system.conf.d/override.conf
-echo "DefaultTimeoutStopSec=15s" | sudo tee -a /etc/systemd/system.conf.d/override.conf
+mkdir -p /etc/systemd/system.conf.d
+echo "[Manager]" | tee /etc/systemd/system.conf.d/override.conf
+echo "DefaultTimeoutStopSec=15s" | tee -a /etc/systemd/system.conf.d/override.conf
 
 # Reload the systemd configuration
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 # Run the setup script
-cd home/
-chmod +x Setup-LMDE-Theme.sh
-./Setup-LMDE-Theme.sh
-cd ..
+# cd home/
+# chmod +x Setup-LMDE-Theme.sh
+# ./Setup-LMDE-Theme.sh
+# cd ..
 
 # Reboot for the changes to take effect
-echo "Installation complete! Please reboot for the changes to take effect."
+echo "Installation complete! Please reboot for the changes to take effect. Then run Setup-LMDE-Theme.sh in cinnamon/home for theming."
