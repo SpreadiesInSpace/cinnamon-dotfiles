1e30eef253643c3cd8aca23e665437a1b4f3b1b2
Spready <f16poom@gmail.com>
2025-03-28 06:13:03 +0700

    Setup Script Fixes

--- unsorted/Fedora/Fedora-Bloat.sh
+++ unsorted/Fedora/Fedora-Bloat.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run the script using sudo."
+  exit
+fi
+
+# Check if the script is run from the root account
+if [ "$SUDO_USER" = "" ]; then
+  echo "Please do not run this script from the root account. Use sudo instead."
+  exit
+fi
+
+# Set GRUB timeout to 0
+sed -i '/^#*GRUB_TIMEOUT=/s/^#*GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
 # Remove Bloat
-sudo dnf remove -y baobab bulky celluloid drawing firefox gnome-calendar gnome-disk-utility hexchat hypnotix mint-artwork mint-backgrounds* mintbackup mintstick mintupdate numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer eom google-noto-seriff* ibus paper-icon-theme pidgin shotwell tracker-miners xawtv xfburn
+dnf remove -y baobab bulky celluloid drawing firefox gnome-calendar hexchat hypnotix mint-artwork mint-backgrounds* mintbackup mintstick mintupdate numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer eom google-noto-seriff* ibus paper-icon-theme pidgin shotwell tracker-miners xawtv xfburn
--- unsorted/LMDE/LMDE-Bloat.sh
+++ unsorted/LMDE/LMDE-Bloat.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run the script using sudo."
+  exit
+fi
+
+# Check if the script is run from the root account
+if [ "$SUDO_USER" = "" ]; then
+  echo "Please do not run this script from the root account. Use sudo instead."
+  exit
+fi
+
+# Set GRUB timeout to 0
+sed -i '/^#*GRUB_TIMEOUT=/s/^#*GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
 # Remove Bloat
-sudo apt remove -y baobab bulky celluloid drawing firefox gnome-calendar gnome-disk-utility hexchat hypnotix mint-artwork mint-backgrounds* mintbackup mintstick numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer mint-L* mint-x* mint-y*
-sudo rm -rf /usr/share/icons/Bibata-* /usr/share/icons/GoogleDot-* /usr/share/icons/XCursor-Pro-* /usr/share/icons/DMZ-* /usr/share/icons/ubuntu-mono-*
+apt remove -y baobab bulky celluloid drawing firefox gnome-calendar hexchat hypnotix mint-artwork mint-backgrounds* mintbackup mintstick numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer mint-L* mint-x* mint-y*
+
+rm -rf /usr/share/icons/Bibata-* /usr/share/icons/GoogleDot-* /usr/share/icons/XCursor-Pro-* /usr/share/icons/DMZ-* /usr/share/icons/ubuntu-mono-*
