91fe46c28dca8ce33575f83527a6fa7df601ab79
Spready <f16poom@gmail.com>
2025-04-11 15:19:17 +0700

    created Setup-ISO.sh

---
+++ extra/Setup-ISO.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+declare -A installs=(
+  [1]="https://tinyurl.com/spready-arch"
+  [2]="https://tinyurl.com/spready-gentoo"
+  [3]="https://tinyurl.com/spready-opensuse-tumbleweed"
+  [4]="https://tinyurl.com/spready-slackware-current"
+  [5]="https://tinyurl.com/spready-void"
+)
+
+declare -A names=(
+  [1]="Arch-Install.sh"
+  [2]="Gentoo-Install.sh"
+  [3]="OpenSUSE-TW-Install.sh"
+  [4]="Slackware-Current-Install.sh"
+  [5]="Void-Install.sh"
+)
+
+# Function to download file
+download_file() {
+  url=$1
+  filename=$2
+
+  if command -v curl &> /dev/null; then
+    echo "Using curl to download $filename..."
+    curl -sL "$url" -o "$filename"
+  elif command -v wget &> /dev/null; then
+    echo "Using wget to download $filename..."
+    wget -q "$url" -O "$filename"
+  else
+    echo "Error: Neither curl nor wget found. Exiting."
+    exit 1
+  fi
+}
+
+echo "Which installer would you like to run?"
+echo "1) Arch Linux"
+echo "2) Gentoo"
+echo "3) openSUSE Tumbleweed"
+echo "4) Slackware Current"
+echo "5) Void Linux"
+read -rp "Enter a number [1-5]: " choice
+
+url=${installs[$choice]}
+filename=${names[$choice]}
+
+if [[ -z "$url" ]]; then
+  echo "Invalid choice. Exiting."
+  exit 1
+fi
+
+echo "Downloading $filename..."
+download_file "$url" "$filename"
+
+chmod +x "$filename"
+echo "Running $filename..."
+./"$filename"
+
