8929b23c5cfbf56460bed4a74ee2a887ca8ea2cc
Spready <f16poom@gmail.com>
2025-04-18 10:43:34 +0700

    suppress script dir output

--- extra/Setup.sh
+++ extra/Setup.sh
@@ -23,7 +23,7 @@ ZIP_NAME="dotfiles.zip"
 EXTRACT_DIR="cinnamon-dotfiles-main"
 
 # Resolve real path of script
-SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0" 2>/dev/null)")" 2>/dev/null && pwd)"
 TOP_DIR="$(dirname "$SCRIPT_DIR")"
 
 # Skip cinnamon-dotfiles download if it already exists
