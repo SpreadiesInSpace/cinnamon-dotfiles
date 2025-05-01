0dea380be9a86c544860031efa64e366b2c417eb
Spready <f16poom@gmail.com>
2025-05-01 17:10:22 +0700

    fix typo

--- extra/brave-gruvbox.sh
+++ extra/brave-gruvbox.sh
@@ -5,7 +5,7 @@ die() { echo -e "\033[1;31mError:\033[0m $*" >&2; exit 1; }
 
 # Backup Existing Brave Profile
 timestamp=$(date +%s)
-[ -d ~/.config/BraveSoftware/ ] mv ~/.config/BraveSoftware/ ~/.config/BraveSoftware.old.$timestamp/ || die "Failed to backup old Brave Profile."
+[ -d ~/.config/BraveSoftware/ ] && mv ~/.config/BraveSoftware/ ~/.config/BraveSoftware.old.$timestamp/ || die "Failed to backup old Brave Profile."
 
 # Clone Brave Gruvbox Example Profile
 git clone https://github.com/spreadiesinspace/BraveSoftware ~/.config/BraveSoftware || die "Failed to download new Brave profile."
