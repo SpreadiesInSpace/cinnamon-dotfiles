ef67666bf81dc16724745ef4e2b0cbdd3b8b5079
Spready <f16poom@gmail.com>
2025-05-02 13:35:21 +0700

    add ca-certificates.sh

---
+++ unsorted/Slackware/ca-certificates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Minimal Error Handling function
+die() { echo -e "\033[1;31mError:\033[0m $*" >&2; exit 1; }
+
+# Remove installed ca-certificates
+sudo slpkg -Ry ca-certificates || die "Failed to remove current ca-certificates."
+
+# Install older ca-certificates
+wget -c -T 10 -t 10 -q --show-progress https://slackware.uk/cumulative/slackware-current/slackware/n/ca-certificates-20250131-noarch-1.txz || die "Failed to download ca-certificates package."
+sudo installpkg ca-certificates-20250131-noarch-1.txz || die "Failed to install ca-certificates package."
+rm ca-certificates-20250131-noarch-1.txz || die "Failed to remove ca-certificates package file."
+
+# Blacklist ca-certificates
+sudo sed -i 's/^\(PACKAGES *= *\["mint"\)\]/\1, "ca-certificates"]/' /etc/slpkg/blacklist.toml
+
