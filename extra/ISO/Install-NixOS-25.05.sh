563a7a7733a480af3b9e01da5ee517a5d8947c59
Spready <f16poom@gmail.com>
2025-07-19 03:17:13 +0700

    don't add nix-channel (already in configuration.nix via Tarball)

--- extra/ISO/Install-NixOS-25.05.sh
+++ extra/ISO/Install-NixOS-25.05.sh
@@ -109,8 +109,8 @@ sed -i "s|^\(\s*time\.timeZone\s*=\s*\).*|\\1\"$timezone\";|" "$CONFIG"|| die "F
 
 # Add Nix Unstable and 23.05 Channels (for Neovim, icons and themes)
 # nix-channel --add https://nixos.org/channels/nixos-unstable nixos || die "Failed to add Nix unstable channel."
-nix-channel --add https://nixos.org/channels/nixos-23.05 nixos-23.05 || die "Failed to add Nix 23.05 channel."
-nix-channel --update || die "Failed to update Nix channels."
+# nix-channel --add https://nixos.org/channels/nixos-23.05 nixos-23.05 || die "Failed to add Nix 23.05 channel."
+# nix-channel --update || die "Failed to update Nix channels."
 
 # Install NixOS
 nixos-install --no-root-passwd || die "Failed to install NixOS."
