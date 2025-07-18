1514000a0cca807ca73e0861fa760afb7850bf99
Spready <f16poom@gmail.com>
2025-07-19 03:05:11 +0700

    remove invalid channel

--- extra/ISO/Install-NixOS-25.05.sh
+++ extra/ISO/Install-NixOS-25.05.sh
@@ -109,7 +109,6 @@ sed -i "s|^\(\s*time\.timeZone\s*=\s*\).*|\\1\"$timezone\";|" "$CONFIG"|| die "F
 
 # Add Nix Unstable and 23.05 Channels (for Neovim, icons and themes)
 # nix-channel --add https://nixos.org/channels/nixos-unstable nixos || die "Failed to add Nix unstable channel."
-nix-channel --add https://nixos.org/channels/nixos-25.05 nixos || die "Failed to add Nix 25.05 channel."
 nix-channel --add https://nixos.org/channels/nixos-23.05 nixos-23.05 || die "Failed to add Nix 23.05 channel."
 nix-channel --update || die "Failed to update Nix channels."
 
