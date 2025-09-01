58750e6f7f8ec21c2bf2ad71402722061519fdfe
Spready <f16poom@gmail.com>
2025-09-01 18:10:31 +0700

    remove firewalld as well in Fedora-Bloat.sh (using ufw)

--- unsorted/Fedora/Fedora-Bloat.sh
+++ unsorted/Fedora/Fedora-Bloat.sh
@@ -19,11 +19,11 @@ systemctl disable --now abrtd.service >/dev/null 2>&1 || true
 
 # Remove Bloat
 dnf remove -y \
-	baobab bulky celluloid drawing firefox gnome-calendar hexchat hp* hypnotix \
-	mint-artwork mint-backgrounds* mintbackup mintstick mintupdate numix* \
-	papirus-icon-theme pix pppoeconf redshift simple-scan thingy thunderbird \
-	transmission-gtk warpinator webapp-manager xed xreader xviewer eom \
-	google-noto-seriff* ibus* paper-icon-theme pidgin shotwell tecla \
+	baobab bulky celluloid drawing firefox firewalld gnome-calendar hexchat hp* \
+	hypnotix mint-artwork mint-backgrounds* mintbackup mintstick mintupdate \
+	numix* papirus-icon-theme pix pppoeconf redshift simple-scan thingy \
+	thunderbird transmission-gtk warpinator webapp-manager xed xreader xviewer \
+	eom google-noto-seriff* ibus* paper-icon-theme pidgin shotwell tecla \
 	tracker-miners xawtv xfburn
 
 # Replace FirewallD with UFW and allow KDE Connect through
