6179c99c7c3ccb5c03d27c6f5eb3a6769d952b80
Spready <f16poom@gmail.com>
2025-12-14 23:20:33 +0700

    switch to Yes as default for Setup-Common.sh display_status function

--- Setup-Common.sh
+++ Setup-Common.sh
@@ -52,10 +52,10 @@ display_status() {
 
   # Confirm before proceeding
   while true; do
-    read -rp "Continue with these settings? [y/N]: " confirm
-    if [[ "$confirm" =~ ^([yY]|[yY][eE][sS])$ ]]; then
+    read -rp "Continue with these settings? [Y/n]: " confirm
+    if [[ "$confirm" =~ ^([yY]|[yY][eE][sS])$ || -z "$confirm" ]]; then
       break
-    elif [[ "$confirm" =~ ^([nN]|[nN][oO])$ || -z "$confirm" ]]; then
+    elif [[ "$confirm" =~ ^([nN]|[nN][oO])$ ]]; then
       die "Setup cancelled by user."
     else
       echo "Invalid input. Please answer y or n."
