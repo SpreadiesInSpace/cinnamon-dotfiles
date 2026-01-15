e9d8aa33429597263e86848483522827190857ae
Spready <f16poom@gmail.com>
2026-01-16 04:02:32 +0700

    add noto-fonts.sh

--- extra/noto-fonts.sh
+++ extra/noto-fonts.sh
@@ -3,28 +3,28 @@
 # Minimal Error Handling function
 die() { echo -e "\033[1;31mError:\033[0m $*" >&2; exit 1; }
 
-# Download noto-fonts-cjk
-URL="https://archlinux.org/packages/extra/any/noto-fonts-cjk/download/"
+# Download noto-fonts
+URL="https://archlinux.org/packages/extra/any/noto-fonts/download/"
 wget -c -T 10 -t 10 -q --show-progress --content-disposition "$URL" || \
-  die "Failed to download noto-fonts-cjk."
+  die "Failed to download noto-fonts."
 
 # Unpack
 zstd -d ./*.pkg.tar.zst || die "Decompression failed."
 tar -xf ./*.pkg.tar || die "Extraction failed."
 
 # Install
-echo "Installing noto-fonts-cjk."
-sudo cp -npr usr/share/fonts/noto-cjk/ /usr/share/fonts/ || \
-  die "Failed to copy noto-fonts-cjk to /usr/share/fonts"
+echo "Installing noto-fonts."
+sudo cp -npr usr/share/fonts/noto/ /usr/share/fonts/ || \
+  die "Failed to copy noto-fonts to /usr/share/fonts"
 
 # Symlink
 mkdir -p ~/.fonts || die "Failed to make .fonts folder."
-sudo ln -sf /usr/share/fonts/noto-cjk ~/.fonts || \
+sudo ln -sf /usr/share/fonts/noto ~/.fonts || \
   die "Failed to symlink fonts."
 
 # Clean Up
 rm -rf usr/ || die "Failed to remove usr/"
-rm -rf noto-fonts-cjk-* || die "Failed to remove noto-fonts-cjk-*"
+rm -rf noto-fonts-* || die "Failed to remove noto-fonts-*"
 rm -rf .BUILDINFO || die "Failed to remove .BUILDINFO"
 rm -rf .PKGINFO || die "Failed to remove .PKGINFO"
 rm -rf .MTREE || die "Failed to remove .MTREE"
