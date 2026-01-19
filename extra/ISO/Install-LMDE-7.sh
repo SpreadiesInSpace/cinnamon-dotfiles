9250eaae104b0620aa4546f73ef97dff44a59ba4
Spready <f16poom@gmail.com>
2026-01-19 10:35:30 +0700

    formatting

--- extra/ISO/Install-LMDE-7.sh
+++ extra/ISO/Install-LMDE-7.sh
@@ -96,7 +96,7 @@ arch-chroot /mnt echo "LANG=en_US.UTF-8" > /etc/locale.conf || \
 # Install System Packages
 retry arch-chroot /mnt bash -c \
   'DEBIAN_FRONTEND=noninteractive apt update' || \
-  die "Failed to install packages."
+  die "APT update failed."
 retry arch-chroot /mnt bash -c \
   'DEBIAN_FRONTEND=noninteractive apt install -y \
   amd64-microcode arch-install-scripts bash-completion \
